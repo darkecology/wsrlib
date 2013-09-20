6c601a4177654452c7c316e5343efd5652a67800
Jeffrey Geevarghese <j3ffreyjohn@gmail.com>
2013-09-20 17:09:08 -0400

    Unit Test for alignSweepsToFixed

---
+++ tst/testAlignSweepsToFixed.m
@@ -0,0 +1,30 @@
+function [ success ] = testAlignSweepsToFixed( dataSetName )
+%testAlignSweepsToFixed Unit Test for the alignSweepsToFixed function. 
+
+scans = getScans(dataSetName);      %get absolute paths of all scans in the dataset
+
+for i=1:size(scans,1),
+    radar_file = scans(i);
+
+    % Parse the filename to get the station
+    scaninfo = wsr88d_scaninfo(radar_file{1});
+        
+    % Construct options for rsl2mat
+    opt = struct();
+    opt.cartesian = false;
+    opt.max_elev = 5;
+        
+    try
+        radar = rsl2mat(radar_file{1}, scaninfo.station, opt);
+        radar_aligned = alignSweepsToFixed(radar,false,'ground');
+        fprintf('%s : Aligned.  \n', radar_file{1});
+    catch err       
+        fprintf('%s : %s \n', radar_file{1}, err.message);
+    end
+end
+
+
+fprintf('Testing successful \n');
+
+end
+
