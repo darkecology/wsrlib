66330a8dc6e21df2ed8bd2fe2ac8053bf7fe1df2
Dan Sheldon <sheldon@cs.umass.edu>
2013-10-22 15:50:44 -0400

    Improve the installation procedures for cajun. These practices should be followed:
    * Run install_cajun.m for one-time setup after checking out the code.
      This should compile all toolboxes and utility functions
    
    * Run setup_cajun.m each MATLAB session. I recommend putting the following
      lines in your personal startup.m file:
    
      addpath('/path/to/cajun');
      setup_cajun.m
    
    * Do not put any additional addpath commands into scripts if you can
      possibly avoid it. Put them in setup_cajun.m instead
    
    * For deployed applications, run separate compilation scripts. If you do
      this from a normal MATLAB session in which you have already run setup_cajun.m,
      you shouldn't need any additional path setup.
    
      (An exception to this is if you run MATLAB 2012a due to the javaaddpath
       issue in that release.)

---
+++ install_cajun.m
@@ -0,0 +1,37 @@
+function install_cajun()
+% INSTALL_CAJUN One-time installation for cajun
+% 
+% Run this script for one-time compilation, etc. of toolboxes used by cajun
+% 
+% If you pull in additional toolboxes that need to be compiled for each
+% machine/platform, add their installation commands below.
+
+cajundir = fileparts(mfilename('fullpath'));
+
+%%%%%%%%%%%%%%%%%%%%%% 
+% rsl
+%%%%%%%%%%%%%%%%%%%%%% 
+fprintf('********** Compiling rsl ************\n\n');
+
+status = system(sprintf('make -C %s/rsl2mat/rsl', cajundir), '-echo');
+if status ~= 0
+    error('Failed to compile rsl');
+end
+
+fprintf('\n\nSuccessfully compiled rsl\n\n');
+
+%%%%%%%%%%%%%%%%%%%%%% 
+% rsl2mat
+%%%%%%%%%%%%%%%%%%%%%% 
+addpath(sprintf('%s/rsl2mat', cajundir));
+make_rsl2mat();
+
+%%%%%%%%%%%%%%%%%%%%%% 
+% lightspeed
+%%%%%%%%%%%%%%%%%%%%%%
+
+fprintf('\n\nCompiling lightspeed toolbox\n\n');
+
+curdir = cd(sprintf('%s/lightspeed/lightspeed', cajundir));
+install_lightspeed;
+cd(curdir);
