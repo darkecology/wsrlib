3bb436e7e6c2404c7521481c2470c1ef80937249
Daniel Sheldon <sheldon@cs.umass.edu>
2014-07-01 13:48:02 -0400

    reoarganization

--- setup_wsrlib.m
+++ setup_wsrlib.m
@@ -1,11 +1,11 @@
-function setup_cajun()
-% SETUP_CAJUN Add paths for cajun codebase
+function setup_wsrlib()
+% SETUP_CAJUN Add paths for wsrlib codebase
 %
 % Best practice: place the following commands in your MATLAB startup.m
 % file:
 %
-% addpath('/path/to/cajun');
-% setup_cajun();
+% addpath('/path/to/wsrlib');
+% setup_wsrlib();
 %
 % If you another subdirectory contains utility functions and should be on
 % the MATLAB path, add it below.
@@ -14,25 +14,25 @@ if isdeployed
     return;
 end
 
-cajundir = fileparts(mfilename('fullpath'));
+root = fileparts(mfilename('fullpath'));
 
 % Add directories non-recursively when possible to avoid slow commands and
 % bloated MATLAB path
-addpath(cajundir);
-addpath(sprintf('%s/lib/m_map', cajundir));
-addpath(sprintf('%s/src/narr', cajundir));
-addpath(sprintf('%s/src/legacy', cajundir)); % Once all bugs are fixed, cajun should no longer depend on methods in this folder
-addpath(sprintf('%s/workflow', cajundir));
-addpath(sprintf('%s/lib/rsl2mat', cajundir));
-addpath(sprintf('%s/tst', cajundir));
-addpath(sprintf('%s/src/radar', cajundir));
-addpath(sprintf('%s/src/util', cajundir));
-addpath(sprintf('%s/src/radar/vvp', cajundir));
-addpath(sprintf('%s/src/util/colormaps', cajundir));
-addpath(sprintf('%s/src/radar/clutterMaps', cajundir));
+addpath(root);
+addpath(sprintf('%s/rsl2mat', root));
+addpath(sprintf('%s/lib/m_map', root));
+
+addpath(sprintf('%s/src/narr', root));
+addpath(sprintf('%s/src/legacy', root)); % Once all bugs are fixed, wsrlib should no longer depend on methods in this folder
+addpath(sprintf('%s/src/tst', root));
+addpath(sprintf('%s/src/radar', root));
+addpath(sprintf('%s/src/util', root));
+addpath(sprintf('%s/src/radar/vvp', root));
+addpath(sprintf('%s/src/util/colormaps', root));
+addpath(sprintf('%s/src/radar/clutterMaps', root));
 
 % Setup netcdf-java toolbox. Includes java files
-addpath(genpath(sprintf('%s/lib/njtbx', cajundir)));
+addpath(genpath(sprintf('%s/lib/njtbx', root)));
 
-javaaddpath(sprintf('%s/lib/njtbx/jar/toolsUI-4.0.49.jar', cajundir),'-end');
-javaaddpath(sprintf('%s/lib/njtbx/jar/njTools-2.0.12_jre1.6.jar', cajundir),'-end');
+javaaddpath(sprintf('%s/lib/njtbx/jar/toolsUI-4.0.49.jar', root),'-end');
+javaaddpath(sprintf('%s/lib/njtbx/jar/njTools-2.0.12_jre1.6.jar', root),'-end');
