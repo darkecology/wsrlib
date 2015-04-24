b2a9f1d5ae92d625ba1d12fa7299ba271676a1ff
Dan Sheldon <sheldon@cs.umass.edu>
2015-04-24 16:09:52 -0400

    Lots of changes. Mostly done with conversion to object-oriented

--- src/weather/trash/nam/nam_xy2ij.m
+++ src/weather/trash/nam/nam_xy2ij.m
@@ -1,9 +1,9 @@
-function [ i, j ] = narr_xy2ij( x, y )
+function [ i, j ] = nam_xy2ij( x, y )
 %NARR_XY2IJ Convert from NARR x,y coordinates to i,j indices into NARR grid
 %
 % Find the closest grid point to given x,y point
 
-s = narr_grid();
+s = nam_grid();
 [i, j] = xy2ij(x, y, s);
 
 end
