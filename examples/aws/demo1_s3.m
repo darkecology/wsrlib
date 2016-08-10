69d3cad9166cd3fd0fff9b2296c0fd32331f33c5
Dan Sheldon <sheldon@cs.umass.edu>
2016-08-10 15:42:25 -0400

    aws example (under test)

---
+++ examples/aws/demo1_s3.m
@@ -0,0 +1,17 @@
+
+station = 'KBOX';
+
+t = datetime(2016, 8, 1, 13, 0, 0);
+fileinfo = aws_list('KBOX', 2014, 8, 01, 23 );
+
+thefile = fileinfo(1);
+system( sprintf('aws s3 cp s3://noaa-nexrad-level2/%s .', thefile.key ) );
+
+% Write file
+radar = rsl2mat(thefile.name, station);
+rmax = 150000;
+dim = 600;
+dzmap = jet(32);
+[z, x, y] = sweep2cart(radar.dz.sweeps(1), rmax, dim);    
+dz_gif = mat2ind(z, [-5 30], dzmap);
+imwrite_gif_nan( dz_gif, dzmap, 'dz.gif' );
