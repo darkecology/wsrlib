3a711a401193aff7e5b9d4e073dbd0ed397dbea7
Dan Sheldon <sheldon@cs.umass.edu>
2019-07-12 16:04:38 -0400

    Improve documentation Contents.m files

--- Contents.m
+++ Contents.m
@@ -2,7 +2,6 @@
 %
 % Files
 %   load_segment_net - Load neural network from file into memory
-%   preprocessScan   - Preprocess rendered scan for segmentation
-%   renderscan       - Render a radar scan for segmentation
 %   segment_scan     - Run segmentation net to segment the scan
+
 
