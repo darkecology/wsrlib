1dd6c40bcfa63c7fba34178535dfb78cdc307edd
Dan Sheldon <sheldon@swarm2.cm.cluster>
2020-12-12 20:54:35 -0500

    Implement the download using command-line to avoid bug in the MATLAB filestore, which hangs fetching a file on the swarm2 cluster

--- src/radar/rsl2mat_s3.m
+++ src/radar/rsl2mat_s3.m
@@ -21,13 +21,43 @@ function radar = rsl2mat_s3(key, varargin)
 %
 % See also: RSL2MAT
 
-    function radar = rsl2matReader(file)
-        radar = rsl2mat(file, varargin{:});
+
+    [~, aws_path, name] = aws_key(key);
+
+    % s3 directory listing to find full name of scan
+    cmd = sprintf('aws s3 ls s3://noaa-nexrad-level2/%s/%s', aws_path, name);
+    [status, result] = system(cmd);
+    if status
+      error('command failed (exit status=%d)\n %s\n %s', status, cmd, result);
     end
 
-    fullkey = aws_key(key);
-    location = sprintf('s3://noaa-nexrad-level2/%s*', fullkey);
-    fds = fileDatastore(location, 'ReadFcn', @rsl2matReader);
+    fields = strsplit(result);
+    fullkey = fields{4};
 
-    radar = read(fds);    
+    local_file = tempname();
+
+    % Copy the scan
+    cmd = sprintf('aws s3 cp s3://noaa-nexrad-level2/%s/%s %s', aws_path, fullkey, local_file);
+    [status, result] = system(cmd);
+    if status
+      error('command failed (exit status=%d)\n %s\n %s', status, cmd, result);
+    end
+
+    radar = rsl2mat(local_file, varargin{:});
+
+    delete(local_file);
+
+
+    % This version is a bit slicker, but doesn't work properly on the swarm2 cluster
+    % possibly due to a bug in the Mathworks implementation
+    % 
+    % function radar = rsl2matReader(file)
+    %     radar = rsl2mat(file, varargin{:});
+    % end
+    %
+    % fullkey = aws_key(key);
+    % location = sprintf('s3://noaa-nexrad-level2/%s', fullkey);
+    % fds = fileDatastore(location, 'ReadFcn', @rsl2matReader);
+    % 
+    % radar = read(fds);
 end
